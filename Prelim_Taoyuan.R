582c8164b9fbef82bc61e85e132a9b9aea6f9d3d
ksnys90 <127408346+ksnys90@users.noreply.github.com>
2023-05-30 15:10:13 +0800

    Update Prelim_Taoyuan.R

--- Prelim_Taoyuan.R
+++ Prelim_Taoyuan.R
@@ -24,11 +24,14 @@ peng2 <- peng
 colnames(zhang2)
 
 #tagging posts that mention birth related terms as 1 and others as 0
-zhang2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢",zhang2$Message),1,0)
-lai2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢",lai2$Message),1,0)
-lin2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢",lin2$Message),1,0)
-peng2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢",peng2$Message),1,0)
+zhang2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢｜少子化｜ 生育率",zhang2$Message),1,0)
+lai2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢｜少子化｜ 生育率",lai2$Message),1,0)
+lin2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢｜少子化｜ 生育率",lin2$Message),1,0)
+peng2$birthkey <- ifelse(grepl("生育|生孩子|孕育|懷孕|育兒|育嬰|新生兒|托嬰|公托|臨托|產檢｜少子化｜ 生育率",peng2$Message),1,0)
 
+
+#the whole facebook - what are they discussing?
+  
 
 #getting a quick summary numeber of the number of posts that mention birth
 zhang2_birth <- zhang2 %>%
@@ -61,5 +64,22 @@ lin2_birth1 <- lin2 %>%
 
 peng2_birth1 <- peng2 %>%
   filter(birthkey==1) 
+
+
+
+
+#filtering the rows that do contain the post that mention birth
+
+zhang2_nobirth <- zhang2 %>%
+  filter(birthkey==0) 
+
+lai2_nobirth <- lai2 %>%
+  filter(birthkey==0) 
+
+lin2_nobirth <- lin2 %>%
+  filter(birthkey==0) 
+
+peng2_nobirth <- peng2 %>%
+  filter(birthkey==0) 
 
             
